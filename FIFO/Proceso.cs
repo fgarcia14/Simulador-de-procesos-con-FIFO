ead1a0b4670c1d79a70d57a5ad4cbc5c850a9060
Fidel Garcia <fidel@MacBook-Pro-de-Fidel.local>
2017-05-16 22:10:21 -0500

    -

---
+++ FIFO/Proceso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+	class Proceso
+	{
+		private int _ciclos;
+
+		public int ciclos { get { return _ciclos; } set { _ciclos = value; } }
+
+		public Proceso(int ciclos)
+		{
+			_ciclos = ciclos;
+		}
+	}
+}
---
+++ FIFO/Simulador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+	class Simulador
+	{
+		private static Random rand;
+		Queue<Proceso> fila;
+		private int procesosAtendidos, ciclosVacios, restantes, sumaRestantes;
+
+		public Simulador()
+		{
+			rand = new Random();
+			fila = new Queue<Proceso>();
+		}
+
+		public String simular()
+		{
+			procesosAtendidos = 0;
+			ciclosVacios = 0;
+			sumaRestantes = 0;
+			for (int i = 0; i < 200; i++)
+			{
+				if (rand.Next(1,5)==3)
+				{
+					Proceso p = new Proceso(rand.Next(4,14));
+					fila.Enqueue(p);
+				}
+				if (fila.Count!=0)
+				{
+					fila.Peek().ciclos--;
+					if (fila.Peek().ciclos==0)
+					{
+						fila.Dequeue();
+						procesosAtendidos++;
+					}
+				}
+				else
+				{
+					ciclosVacios++;
+				}
+			}
+			restantes = fila.Count;
+			while (fila.Count!=0)
+			{
+				sumaRestantes+= fila.Dequeue().ciclos;
+			}
+
+			return "Procesos atendidos: " + procesosAtendidos + Environment.NewLine + "Ciclos vacios: " + ciclosVacios + Environment.NewLine + "Procesos faltantes: " + restantes + Environment.NewLine + "Suma restantes: " + sumaRestantes;
+		}
+	}
+}
